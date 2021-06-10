6542fc8feb8527a5aa936ce6092613fa2061962c
lcsm29 <lcsm29@outlook.com>
2021-06-10 10:38:43 +0900

    replaced problem 3 to one liner it has two lines, but line 1 isn't neccessary for the grader

--- ps1/problem_3.py
+++ ps1/problem_3.py
@@ -1,5 +1,2 @@
-cur = lng = s[0]
-for c in s[1:]:
-    cur = '' if c < cur[-1] else cur
-    lng, cur = max(lng, cur + c, key=len), cur + c
-print('Longest substring in alphabetical order is:', lng)
+print('Longest substring in alphabetical order is:', end=' ')
+print(max((''.join([a + ' ' if a > b else a for a, b in zip(s, s[1:] + s)])).split(), key=len))
