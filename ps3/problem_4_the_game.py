cf688b6d879329a5ef35f4af518d20872308e853
lcsm29 <lcsm29@outlook.com>
2021-06-17 00:02:35 +0900

    Add problem 4 implementation is done but removed the code won't publish the solution before the deadline of this problem set in order not to break the honor code

---
+++ ps3/problem_4_the_game.py
@@ -0,0 +1,34 @@
+#   Problem 3 - Printing Out all Available Letters
+#   
+#   Released    2021-06-16 14:00 UTC+00
+#   Started     2021-06-16 14:27 UTC+00
+#   Finished    2021-06-16 14:51 UTC+00
+#
+#               w r i t t e n    b y
+#   oooo        https://github.com/lcsm29        .oooo.    .ooooo.
+#   `888                                       .dP""Y88b  888' `Y88.
+#    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
+#    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
+#    888  888       `"Y88b.   888   888   888    .dP'           888'
+#    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
+#   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
+def hangman(secretWord):
+    '''
+    secretWord: string, the secret word to guess.
+
+    Starts up an interactive game of Hangman.
+
+    * At the start of the game, let the user know how many 
+      letters the secretWord contains.
+
+    * Ask the user to supply one guess (i.e. letter) per round.
+
+    * The user should receive feedback immediately after each guess 
+      about whether their guess appears in the computers word.
+
+    * After each round, you should also display to the user the 
+      partially guessed word so far, as well as letters that the 
+      user has not yet guessed.
+
+    Follows the other limitations detailed in the problem write-up.
+    '''
