f8cbe2ff99ba3183d3d9686fece9204155697b8f
lcsm29 <lcsm29@outlook.com>
2021-06-17 00:02:20 +0900

    Add problem 2 implementation is done but removed the code won't publish the solution before the deadline of this problem set in order not to break the honor code

---
+++ ps3/problem_2_getting_the_users_guess.py
@@ -0,0 +1,21 @@
+#   Problem 2 - Getting the User's Guess
+#   
+#   Released    2021-06-16 14:00 UTC+00
+#   Started     2021-06-16 14:07 UTC+00
+#   Finished    2021-06-16 14:09 UTC+00
+#
+#               w r i t t e n    b y
+#   oooo        https://github.com/lcsm29        .oooo.    .ooooo.
+#   `888                                       .dP""Y88b  888' `Y88.
+#    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
+#    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
+#    888  888       `"Y88b.   888   888   888    .dP'           888'
+#    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
+#   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
+def getGuessedWord(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secretWord have been guessed so far.
+    '''
