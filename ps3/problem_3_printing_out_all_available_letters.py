320591a51bb33e5c371feee5c5a6a5d03a521b10
lcsm29 <lcsm29@outlook.com>
2021-06-17 00:02:29 +0900

    Add problem 3 implementation is done but removed the code won't publish the solution before the deadline of this problem set in order not to break the honor code

---
+++ ps3/problem_3_printing_out_all_available_letters.py
@@ -0,0 +1,20 @@
+#   Problem 3 - Printing Out all Available Letters
+#   
+#   Released    2021-06-16 14:00 UTC+00
+#   Started     2021-06-16 14:10 UTC+00
+#   Finished    2021-06-16 14:12 UTC+00
+#
+#               w r i t t e n    b y
+#   oooo        https://github.com/lcsm29        .oooo.    .ooooo.
+#   `888                                       .dP""Y88b  888' `Y88.
+#    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
+#    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
+#    888  888       `"Y88b.   888   888   888    .dP'           888'
+#    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
+#   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
+def getAvailableLetters(lettersGuessed):
+    '''
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters that represents what letters have not
+      yet been guessed.
+    '''
