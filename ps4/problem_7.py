4e1591f6edc5e7c53167586f58b5353e201d97aa
lcsm29 <lcsm29@outlook.com>
2021-07-01 01:21:46 +0900

    Initial commit: PS4 - Problem 7 - You and your Computer I've implemented the solution for this problem, but I'm commiting this without the solution, in order not to break the honor code. I'll commit the solution after the due date of this problem set.

---
+++ ps4/problem_7.py
@@ -0,0 +1,40 @@
+#   Problem Set 4 - Problem 7 - You and your Computer
+#
+#   Released    2021-06-30 14:00 UTC+00
+#   Started     2021-06-30 15:42 UTC+00
+#   Finished    2021-06-30 15:55 UTC+00
+#   https://github.com/lcsm29/edx-mit-6.00.1x
+#
+#   oooo                w r i t t e n   b y      .oooo.    .ooooo.
+#   `888                                       .dP""Y88b  888' `Y88.
+#    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
+#    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
+#    888  888       `"Y88b.   888   888   888    .dP'           888'
+#    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
+#   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
+def playGame(wordList):
+    """
+    Allow the user to play an arbitrary number of hands.
+ 
+    1) Asks the user to input 'n' or 'r' or 'e'.
+        * If the user inputs 'e', immediately exit the game.
+        * If the user inputs anything that's not 'n', 'r', or 'e', keep asking them again.
+
+    2) Asks the user to input a 'u' or a 'c'.
+        * If the user inputs anything that's not 'c' or 'u', keep asking them again.
+
+    3) Switch functionality based on the above choices:
+        * If the user inputted 'n', play a new (random) hand.
+        * Else, if the user inputted 'r', play the last hand again.
+          But if no hand was played, output "You have not played a hand yet. 
+          Please play a new hand first!"
+        
+        * If the user inputted 'u', let the user play the game
+          with the selected hand, using playHand.
+        * If the user inputted 'c', let the computer play the 
+          game with the selected hand, using compPlayHand.
+
+    4) After the computer or user has played the hand, repeat from step 1
+
+    wordList: list (string)
+    """
