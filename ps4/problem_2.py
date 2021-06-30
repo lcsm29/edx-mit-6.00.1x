8c7a2536373ceba82b52645150995a459229e072
lcsm29 <lcsm29@outlook.com>
2021-07-01 01:15:53 +0900

    Initial commit: PS4 - Problem 2 - Dealing with Hands I've implemented the solution for this problem, but I'm commiting this without the solution, in order not to break the honor code. I'll commit the solution after the due date of this problem set.

---
+++ ps4/problem_2.py
@@ -0,0 +1,30 @@
+#   Problem Set 4 - Problem 2 - Dealing with Hands
+#
+#   Released    2021-06-30 14:00 UTC+00
+#   Started     2021-06-30 14:05 UTC+00
+#   Finished    2021-06-30 14:19 UTC+00
+#   https://github.com/lcsm29/edx-mit-6.00.1x
+#
+#   oooo                w r i t t e n   b y      .oooo.    .ooooo.
+#   `888                                       .dP""Y88b  888' `Y88.
+#    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
+#    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
+#    888  888       `"Y88b.   888   888   888    .dP'           888'
+#    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
+#   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
+def updateHand(hand, word):
+    """
+    Assumes that 'hand' has all the letters in word.
+    In other words, this assumes that however many times
+    a letter appears in 'word', 'hand' has at least as
+    many of that letter in it. 
+
+    Updates the hand: uses up the letters in the given word
+    and returns the new hand, without those letters in it.
+
+    Has no side effects: does not modify hand.
+
+    word: string
+    hand: dictionary (string -> int)    
+    returns: dictionary (string -> int)
+    """
