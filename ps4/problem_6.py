b7921eedf19b66acfdf63b063ce95783d202d905
lcsm29 <lcsm29@outlook.com>
2021-07-01 01:20:58 +0900

    Initial commit: PS4 - Problem 6 - Playing a Game I've implemented the solution for this problem, but I'm commiting this without the solution, in order not to break the honor code. I'll commit the solution after the due date of this problem set.

---
+++ ps4/problem_6.py
@@ -0,0 +1,26 @@
+#   Problem Set 4 - Problem 6 - Playing a Game
+#
+#   Released    2021-06-30 14:00 UTC+00
+#   Started     2021-06-30 15:31 UTC+00
+#   Finished    2021-06-30 15:42 UTC+00
+#   https://github.com/lcsm29/edx-mit-6.00.1x
+#
+#   oooo                w r i t t e n   b y      .oooo.    .ooooo.
+#   `888                                       .dP""Y88b  888' `Y88.
+#    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
+#    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
+#    888  888       `"Y88b.   888   888   888    .dP'           888'
+#    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
+#   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
+def playGame(wordList):
+    """
+    Allow the user to play an arbitrary number of hands.
+ 
+    1) Asks the user to input 'n' or 'r' or 'e'.
+      * If the user inputs 'n', let the user play a new (random) hand.
+      * If the user inputs 'r', let the user play the last hand again.
+      * If the user inputs 'e', exit the game.
+      * If the user inputs anything else, tell them their input was invalid.
+ 
+    2) When done playing the hand, repeat from step 1
+    """
