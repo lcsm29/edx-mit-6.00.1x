31ebeaaa89b14a9dc5798fd060692671697e5fc0
lcsm29 <lcsm29@outlook.com>
2021-07-01 01:17:42 +0900

    Initial commit: PS4 - Problem 3 - Valid Words I've implemented the solution for this problem, but I'm commiting this without the solution, in order not to break the honor code. I'll commit the solution after the due date of this problem set.

---
+++ ps4/problem_3.py
@@ -0,0 +1,25 @@
+#   Problem Set 4 - Problem 3 - Valid Words
+#
+#   Released    2021-06-30 14:00 UTC+00
+#   Started     2021-06-30 14:20 UTC+00
+#   Finished    2021-06-30 14:26 UTC+00
+#   https://github.com/lcsm29/edx-mit-6.00.1x
+#
+#   oooo                w r i t t e n   b y      .oooo.    .ooooo.
+#   `888                                       .dP""Y88b  888' `Y88.
+#    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
+#    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
+#    888  888       `"Y88b.   888   888   888    .dP'           888'
+#    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
+#   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
+def isValidWord(word, hand, wordList):
+    """
+    Returns True if word is in the wordList and is entirely
+    composed of letters in the hand. Otherwise, returns False.
+
+    Does not mutate hand or wordList.
+   
+    word: string
+    hand: dictionary (string -> int)
+    wordList: list of lowercase strings
+    """
