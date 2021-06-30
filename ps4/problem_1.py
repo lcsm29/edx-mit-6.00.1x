1a063db1e4354dc0b65cb180b4738fae181dd646
lcsm29 <lcsm29@outlook.com>
2021-07-01 01:14:51 +0900

    Initial commit: PS4 - Problem 1 - Word Scores I've implemented the solution for this problem, but I'm commiting this without the solution, in order not to break the honor code. I'll commit the solution after the due date of this problem set.

---
+++ ps4/problem_1.py
@@ -0,0 +1,29 @@
+#   Problem Set 4 - Problem 1 - Word Scores
+#
+#   Released    2021-06-30 14:00 UTC+00
+#   Started     2021-06-30 14:02 UTC+00
+#   Finished    2021-06-30 14:05 UTC+00
+#   https://github.com/lcsm29/edx-mit-6.00.1x
+#
+#   oooo                w r i t t e n   b y      .oooo.    .ooooo.
+#   `888                                       .dP""Y88b  888' `Y88.
+#    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
+#    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
+#    888  888       `"Y88b.   888   888   888    .dP'           888'
+#    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
+#   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
+def getWordScore(word, n):
+    """
+    Returns the score for a word. Assumes the word is a valid word.
+
+    The score for a word is the sum of the points for letters in the
+    word, multiplied by the length of the word, PLUS 50 points if all n
+    letters are used on the first turn.
+
+    Letters are scored as in Scrabble; A is worth 1, B is worth 3, C is
+    worth 3, D is worth 2, E is worth 1, and so on (see SCRABBLE_LETTER_VALUES)
+
+    word: string (lowercase letters)
+    n: integer (HAND_SIZE; i.e., hand size required for additional points)
+    returns: int >= 0
+    """
