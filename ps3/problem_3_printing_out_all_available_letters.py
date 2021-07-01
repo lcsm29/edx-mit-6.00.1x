e4bb4a639807a218dc04d38abc00482ac7243674
lcsm29 <lcsm29@outlook.com>
2021-07-02 08:35:49 +0900

    Update Solution for Problem Set 3, Problem 3

--- ps3/problem_3_printing_out_all_available_letters.py
+++ ps3/problem_3_printing_out_all_available_letters.py
@@ -1,11 +1,11 @@
-#   Problem 3 - Printing Out all Available Letters
+#   Problem Set 3 - Problem 3 - Printing Out all Available Letters
 #   
 #   Released    2021-06-16 14:00 UTC+00
 #   Started     2021-06-16 14:10 UTC+00
 #   Finished    2021-06-16 14:12 UTC+00
+#   https://github.com/lcsm29/edx-mit-6.00.1x
 #
-#               w r i t t e n    b y
-#   oooo        https://github.com/lcsm29        .oooo.    .ooooo.
+#   oooo                w r i t t e n   b y      .oooo.    .ooooo.
 #   `888                                       .dP""Y88b  888' `Y88.
 #    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
 #    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
@@ -18,3 +18,4 @@ def getAvailableLetters(lettersGuessed):
     returns: string, comprised of letters that represents what letters have not
       yet been guessed.
     '''
+    return string.ascii_lowercase if not lettersGuessed else ''.join([c for c in string.ascii_lowercase if c not in lettersGuessed])
