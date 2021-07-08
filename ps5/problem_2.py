7f1c1cd6588f0692b8a7631f217b949abf52dcea
lcsm29 <lcsm29@outlook.com>
2021-07-08 13:41:14 +0900

    Initial Commit: PS5 - Problem 1, 2, 3, 4 I've implemented the solution for this problem, but this commit does not contains the solution, in order not to break the honor code. I'll commit the solution after the due date of this problem set.

---
+++ ps5/problem_2.py
@@ -0,0 +1,14 @@
+#   Problem Set 5 - Problem 2 - PlaintextMessage
+#
+#   Released    2021-07-07 14:00 UTC+00
+#   Started     2021-07-08 04:21 UTC+00
+#   Finished    2021-07-08 04:25 UTC+00
+#   https://github.com/lcsm29/edx-mit-6.00.1x
+#
+#   oooo                w r i t t e n   b y      .oooo.    .ooooo.
+#   `888                                       .dP""Y88b  888' `Y88.
+#    888   .ooooo.   .oooo.o ooo. .oo.  .oo.         ]8P' 888    888
+#    888  d88' `"Y8 d88(  "8 `888P"Y88bP"Y88b      .d8P'   `Vbood888
+#    888  888       `"Y88b.   888   888   888    .dP'           888'
+#    888  888   .o8 o.  )88b  888   888   888  .oP     .o     .88P'
+#   o888o `Y8bod8P' 8""888P' o888o o888o o888o 8888888888   .oP'
